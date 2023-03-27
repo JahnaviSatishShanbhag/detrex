57cf2b35b87917f798b54330ce0e017120c39032
JahnaviSatishShanbhag <jahnavisatish.cs19@bmsce.ac.in>
2023-03-27 11:27:37 +0530

    added detectron2

---
+++ detectron2/detectron2/engine/trainMultiGpu.py
@@ -0,0 +1,13 @@
+from tools import train_net
+from detectron2.engine import launch
+
+def train_multi_gpu():
+     args = default_argument_parser().parse_args()
+     launch(
+        main,
+        num_gpus_per_machine=2,
+        num_machines=1,
+        machine_rank=0,
+        dist_url="auto",
+        args=(args,),
+    )
